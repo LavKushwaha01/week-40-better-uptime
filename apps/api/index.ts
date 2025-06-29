dae99a31c9a575210d6948eec96b086fc3fcf05e
harkirat <harkirat96@gmail.com>
2025-06-29 18:59:21 +0530

    Added crud fuinctionality

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,11 +1,14 @@
-
+// require("dotenv").config();
+import jwt from "jsonwebtoken";
 import express from "express"
 const app = express();
 import { prismaClient } from "store/client";
+import { AuthInput } from "./types";
+import { authMiddleware } from "./middleware";
 
 app.use(express.json());
 
-app.post("/website", async (req, res) => {
+app.post("/website", authMiddleware, async (req, res) => {
     if (!req.body.url) {
         res.status(411).json({});
         return
@@ -13,7 +16,8 @@ app.post("/website", async (req, res) => {
     const website = await prismaClient.website.create({
         data: {
             url: req.body.url,
-            timeAdded: new Date()
+            time_added: new Date(),
+            user_id: req.userId!
         }
     })
 
@@ -22,9 +26,87 @@ app.post("/website", async (req, res) => {
     })
 });
 
-app.get("/status/:websiteId", (req, res) => {
+app.get("/status/:websiteId", authMiddleware, async (req, res) => {
+    const website = await prismaClient.website.findFirst({
+        where: {
+            user_id: req.userId!,
+            id: req.params.websiteId,
+        },
+        include: {
+            ticks: {
+                orderBy: [{
+                    createdAt: 'desc',
+                }],
+                take: 1
+            }
+        }
+    })
+
+    if (!website) {
+        res.status(409).json({
+            message: "Not found"
+        })
+        return;
+    }
+
+    res.json({
+        url: website.url,
+        id: website.id,
+        user_id: website.user_id
+    })
+
+})
+
+app.post("/user/signup", async (req, res) => {
+    const data = AuthInput.safeParse(req.body);
+    if (!data.success) {
+        console.log(data.error.toString());
+        res.status(403).send("");
+        return;
+    }
+
+    try {
+        let user = await prismaClient.user.create({
+            data: {
+                username: data.data.username,
+                password: data.data.password
+            }
+    })
+        res.json({
+            id: user.id
+        })
+    } catch(e) {
+        console.log(e);
+        res.status(403).send("");
+    }
+})
+
+app.post("/user/signin", async (req, res) => {
+    const data = AuthInput.safeParse(req.body);
+    if (!data.success) {
+        res.status(403).send("");
+        return;
+    }
+
+    let user = await prismaClient.user.findFirst({
+        where: {
+            username: data.data.username
+        }
+    })
+
+    if (user?.password !== data.data.password) {
+        res.status(403).send("");
+        return;
+    }
+
+    let token = jwt.sign({
+        sub: user.id
+    }, process.env.JWT_SECRET!)
 
 
+    res.json({
+        jwt: token
+    })
 })
 
 app.listen(process.env.PORT || 3001);
